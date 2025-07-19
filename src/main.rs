ffc0b727971dab286264269d22f651f6ec286ce0
tttiw <witt_uni@outlook.com>
2025-07-19 13:33:02 +0800

    update repl help

--- src/main.rs
+++ src/main.rs
@@ -89,42 +89,18 @@ fn main() {
 }
 
 fn show_help() {
-    const MAN: &str = r#"//basics:
-    x:42
-    y:(1;2;3);y[0]
-    2*til 5            //(0;2;4;6;8)
-    til 3+til 2        //(0;2;2)
-//fn:
-    f:{x+y}            // implicit params
-    f[3;4;]            // call (trailing ;)
-    g:{[a;b]a*b}       // explicit params
-    g[5;6;]
-//cond:
-    $[true;42;99]      // if
-    5<=5
-    5~3                // !=. true
-    3<5
-//your plain fib:
-    fibtr:{[n;a;b]$[n=0;a;fibtr[n-1;b;a+b;]]}
-    fib:{[n]fibtr[n;0;1;]}
-    fib[10;]
-//builtins:
-    count x
-    sum x max x min x
-    first x last x
-    sort x reverse x
-    til n              // 0..n-1
-    abs x sqrt x
-    type x
+    println!(
+        "{}",
+        r#"//builtins:
+  abs neg signum sqrt exp log
+  floor ceiling count first last
+  reverse sum max min avg
+  til range type string
+  take drop where distinct sort
+  and or not
 //repl cmds:
-    help
-    vars
-    clear           // clear all vars
-    load script.wq  // execute script silently, show new bindings
-    quit
-    \h \v \c \l \q"#;
-
-    println!("{}", MAN);
+  help vars clear load quit"#
+    );
 }
 
 fn load_script(evaluator: &mut Evaluator, filename: &str) {
